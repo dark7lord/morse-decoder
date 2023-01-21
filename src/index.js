245e36c379d55d03435e5fd2fa7b3ef2ba1ec987
gopal <gopal@student.21-school.ru>
2023-01-21 19:30:42 +0300

    finish exercise

--- src/index.js
+++ src/index.js
@@ -37,8 +37,44 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+const parseNum2Morse = (str) => {
+    const table = {
+        '10': '.',
+        '11': '-',
+        "00": "",
+        "**": " "
+    };
+
+    const result = [...str].map((x, i, arr) => {
+        if (i % 2 == 0) {
+            const pairNum = x + arr[i + 1];
+            const morzeSym = table[pairNum];
+            return morzeSym;
+        }
+    })
+
+    return result.join('');
+}
+
 function decode(expr) {
-    // write your solution here
+    const arrLetters = expr.split('');
+    let countLetters = arrLetters.length / 10;
+    
+    const arrMorzeSyms = [];
+    while (countLetters-- > 0) {
+        let arrNums = arrLetters.splice(0, 10).join('');
+        arrMorzeSyms.push(parseNum2Morse(arrNums));
+    }
+
+
+    const table = {
+        ...MORSE_TABLE,
+        '     ': ' '
+    }
+
+    const result = arrMorzeSyms.map(x => table[x]);
+
+    return result.join('');
 }
 
 module.exports = {
